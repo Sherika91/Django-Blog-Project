eb2f8d011e0f5330aaa0273c875dc34821a75d0a
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2023-12-20 09:44:43 +0300

    Add comment functionality to blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
@@ -88,6 +89,28 @@ def post_share(request, post_id):
 
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post,
-                                                    'form': form,
-                                                    'sent': sent})
+    return render(request, 'blog/post/share.html',
+                  {'post': post,
+                   'form': form,
+                   'sent': sent})
+
+
+@require_POST
+def post_commnet(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(request, 'blog/post/comments.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
