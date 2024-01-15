016d1352a97c9bdbbceea873d1008d784d35b79b
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-01-15 18:38:39 +0300

    Connected to DB postgresql/Added database settings

--- config/settings.py
+++ config/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "blog",
+        'USER': 'postgres',
+        'PASSWORD': 'luka',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
